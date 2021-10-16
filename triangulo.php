4ce3736a953bb9c5d7f1036ac58e56ddf733b1fe
Andresaerg <86579065+Andresaerg@users.noreply.github.com>
2021-10-16 15:54:20 -0400

    Añadido "estilo"

--- triangulo.php
+++ triangulo.php
@@ -7,6 +7,24 @@
     <title>Calculo de Hipotenusa</title>
 </head>
 <body>
+
+    <style>
+    body {
+        background-image: url(../background-stain-shadow-color-wallpaper-preview.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        font-family:Verdana, Geneva, Tahoma, sans-serif;
+        color: white;
+    }
+
+    table{
+
+    background-color: rgb(35, 121, 121);
+    border-radius: 5px;
+    color: white;
+
+    }
+    </style>
     
     <h1 align="center">Cálculo de la Hipotenusa de un triángulo rectángulo</h1>
     <br>
@@ -51,6 +69,7 @@
     // Realice un programa en PHP que permita calcular la hipotenusa del
     // triangulo rectángulo de lados 3 cm y 4 cm
 
+    //Para calcular por medio de entradas:
     if(isset($_POST['btn']) && $_POST['btn'] == 'Calcular'){
 
         $ca = $_POST['ca'];
@@ -60,10 +79,12 @@
         if(!empty($ca) && !empty($co) && $ca>0 && $co>0){
 
                 $h = sqrt(pow($ca,2) + pow($co,2));
-                echo "<h2 align='center'> Datos Ingresados: </h2>";
-                echo "<h4 align='center'> Cateto Adyacente = ".$ca."</h4>";
-                echo "<h4 align='center'> Cateto Opuesto = " .$co."</h4>";
-                echo "<h4 align='center'> Por lo tanto la Hipotenusa es = ".$h."<h4>";
+                echo "<table align='center' border='2'>";
+                echo "<caption align='top'> Datos Ingresados: </caption>";
+                echo "<tr><td align='center'> Cateto Adyacente = ".$ca."</tr>";
+                echo "<tr><td align='center'> Cateto Opuesto = " .$co."</tr>";
+                echo "<tr><td align='center'> Por lo tanto la Hipotenusa es = ".$h."</tr>";
+                echo "</table>";
 
         } elseif(!empty($ca) && !empty($co) && ($ca<0 || $co<0)){
 
@@ -78,6 +99,7 @@
 
     }
 
+    //Para calcular con los valores predeterminados:
     elseif(isset ($_POST['btnp']) && $_POST['btnp'] == 'Calcular'){
 
         $cap = 3;
@@ -85,10 +107,12 @@
         $hp = null;
 
         $hp = sqrt(pow($cap,2) + pow($cop,2));
-        echo "<h2 align='center'> Datos: </h2>";
-        echo "<h4 align='center'> Cateto Adyacente = ".$cap."</h4>";
-        echo "<h4 align='center'> Cateto Opuesto = " .$cop."</h4>";
-        echo "<h4 align='center'> Por lo tanto la Hipotenusa es = ".$hp."<h4>";
+        echo "<table align='center' border='2'>";
+        echo "<caption align='top'> Datos: </caption>";
+        echo "<tr><td align='center'> Cateto Adyacente = ".$cap."</tr>";
+        echo "<tr><td align='center'> Cateto Opuesto = " .$cop."</tr>";
+        echo "<tr><td align='center'> Por lo tanto la Hipotenusa es = ".$hp."</tr>";
+        echo "</table>";
 
     }
 
